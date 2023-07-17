8f6098e916b7012826f8ea189982038eb39e6a79
IsmaelICG <Ismael.corrales.godoy.icg@gmail.com>
2023-07-17 14:23:55 +0200

    Nuevo color

--- src/Error404.tsx
+++ src/Error404.tsx
@@ -11,7 +11,7 @@ const Error404 = () => {
 <body class="bg-gray-100">
     <div class="flex items-center justify-center h-screen">
         <div class="text-center">
-            <div class="text-red-600 text-6xl font-bold">404</div>
+            <div class="text-red-200 text-6xl font-bold">404</div>
             <div class="text-gray-600 text-xl font-medium mb-6">Página no encontrada</div>
             <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Volver al Inicio</a>
         </div>
