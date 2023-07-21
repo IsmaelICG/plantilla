e46c7001798829d85216a517508eaa4ceceb22b4
IsmaelICG <Ismael.corrales.godoy.icg@gmail.com>
2023-07-21 14:46:49 +0200

    21_07_2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Header } from "./componentes/moleculis/Cabecera/Header.component";
 import { useNavigate } from "react-router-dom";
+import img from './assets/img/1HTML.png';
+import img1 from './assets/img/2JAVA.png';
+import img2 from './assets/img/3SQL.png';
+import img3 from './assets/img/3067287.png';
 
 const App = () => {
   const navigate = useNavigate();
@@ -24,7 +28,7 @@ const App = () => {
         </div>
         <div className="flex justify-center md:justify-end w-full md:w-1/2">
           <img
-            src="assets/img/3067287.png"
+            src={img3}
             alt="Computer"
             className="h-64 md:h-80 w-64 md:w-80 mt-10 md:mt-20 mr-10 md:mr-20"
           ></img>
@@ -38,15 +42,15 @@ const App = () => {
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 mt-10 md:mt-20 mx-5 md:mx-20">
         <div onClick={() => navigate("/upss")} className="text-center">
-          <img src="assets/img/1HTML.png" alt="HTML"></img>
+          <img src={img} alt="HTML"></img>
           <h1 className="mt-1 text-xl md:text-3xl font-bold">HTML</h1>
         </div>
         <div onClick={() => navigate("/upss")} className="text-center">
-          <img src="assets/img/2JAVA.png" alt="Java"></img>
+          <img src={img1} alt="Java"></img>
           <h1 className="mt-1 text-xl md:text-3xl font-bold">Java</h1>
         </div>
         <div onClick={() => navigate("/upss")} className="text-center">
-          <img src="assets/img/3SQL.png" alt="SQL"></img>
+          <img src={img2} alt="SQL"></img>
           <h1 className="mt-1 text-xl md:text-3xl font-bold">SQL</h1>
         </div>
       </div>
